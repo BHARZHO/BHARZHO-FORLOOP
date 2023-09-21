9fa3221b60458ec0a0eb229c8b0a436a9c3b29d4
Abdulsalam Abdulbasit <abdulbasitabdulsalam@gmail.com>
2023-09-21 02:20:23 -0700

    This is my first for loop project

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿/*
+diffrent types of looping
+1.while loop
+2.dual loop
+3.for loop
+*/
+
+// int counter = 1;
+// while (counter <= 10)
+
+// {
+//     counter++;
+//     Console.WriteLine(counter);
+
+// }
+
+// int counter = 11; 
+
+// do
+// {
+//     Console.WriteLine(counter);
+//     counter++;
+// } while (counter <= 15 );
+// int counter = 11; 
+
+// do
+// {
+//     Console.WriteLine(counter);
+//     counter--;
+// } while (counter >= 1 );
+
+// Console.Write("Enter your multiplier no:");
+// int number =  int.Parse(Console.ReadLine()!);
+
+// Console.Write("Enter your tablelenght:");
+// int tableLenght = int.Parse(Console.ReadLine()!);
+
+// for (int i = 1; i <= tableLenght; i++)
+// {
+//     int result = number * i;
+//     Console.WriteLine($"{number} x {i} = {result}");
+// }
